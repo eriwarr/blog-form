119d1c45e4d9c0ec28b5bc8fe9b1038a1492f863
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-06-07 22:10:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogList: [], 
+    }
+  }
+
+  componentDidMount() {
+    const blogList = [
+      {
+        url: 'huffingtonpost.com', title: 'Huffington Post', tag: '',
+      },
+      {
+        url: 'boingboing.net', title: 'Boing Boing', tag: '',
+      },
+      {
+        url: 'techcrunch.com', title: 'Techcrunch', tag: '',
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <>
+        <form action="">
+          <input type="text" placeholder="Enter the blog's url"/>
+          <input type="text" placeholder="Enter the blog's title"/>
+          <input type="text" placeholder="Enter #tag"/>
+        </form>
+      </>
+    )
+  }
 }
 
 export default App;
